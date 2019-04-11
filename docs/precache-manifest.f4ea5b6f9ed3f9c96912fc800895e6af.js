5cbf1d4deffa3480ab8afe7251c81e66c52e3f35
Haowen <haowen737@gmail.com>
2019-04-11 20:55:14 +0800

    Update docs

---
+++ docs/precache-manifest.f4ea5b6f9ed3f9c96912fc800895e6af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b8a078906b496315e317",
+    "url": "/static/js/main.7d37d5b5.chunk.js"
+  },
+  {
+    "revision": "705252b29f280db51b22",
+    "url": "/static/js/2.2af8cd3b.chunk.js"
+  },
+  {
+    "revision": "b8a078906b496315e317",
+    "url": "/static/css/main.1fc08804.chunk.css"
+  },
+  {
+    "revision": "5e329f4eb83ff306597fe15dd26ea9ca",
+    "url": "/index.html"
+  }
+];
--- example/src/App.js
+++ example/src/App.js
@@ -1,36 +1,27 @@
 import React from 'react'
-import { useState, useEffect, useRef } from "react"
+import { useState } from "react"
 import logo from './logo.svg'
 import './App.css'
 import useTypewriter from "../../build"
 
-const alarmClock = () => new Date().toLocaleTimeString()
-
 export default function App() {
-  const [time, setTime] = useState(alarmClock())
-  const intervalRef = useRef({})
+  const [value, setValue] = useState('')
+  const [time, setTime] = useState('')
   const typing = useTypewriter(time)
-
-  useEffect(() => {
-      intervalRef.current = setInterval(() => {
-        setTime(alarmClock())
-      }, 3000)
-      return function clear() {
-        clearInterval(intervalRef.current)
-      }
-    },
-    [time]
-  )
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {typing}
-      </header>
+      <header>react-typewriter-hook</header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>{typing}</p>
+      <textarea
+        rows="4"
+        cols="50"
+        onChange={(ev) => setValue(ev.target.value)}
+        value={value}
+        placeholder="write something interesting and click start"
+        />
+      <button onClick={() => setTime(value)}>Start!</button>
     </div>
   )
 }
